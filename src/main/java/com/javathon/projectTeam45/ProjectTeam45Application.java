ba31ded02b6df89c5a31a023c756aaf400867fbb
nekitos911 <n.i.k.tver@yandex.ru>
2019-01-26 16:15:41 +0300

    init commit

---
+++ src/main/java/com/javathon/projectTeam45/ProjectTeam45Application.java
@@ -0,0 +1,14 @@
+package com.javathon.projectTeam45;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTeam45Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTeam45Application.class, args);
+	}
+
+}
+
