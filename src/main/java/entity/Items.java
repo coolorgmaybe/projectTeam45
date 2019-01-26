b0561caa55458310b86f2b3d95a729692b046e97
Timofey <gusev.t.s1@gmail.com>
2019-01-26 19:44:23 +0300

    added data model (#3)

---
+++ src/main/java/entity/Items.java
@@ -0,0 +1,31 @@
+package entity;
+
+import javax.persistence.Entity;
+
+/**
+ * This is list of items. Every item is a uniq element.
+ */
+
+@Entity
+public class Items {
+
+    private String id;
+
+    private String itemsName;
+
+    public String getItemsName() {
+        return itemsName;
+    }
+
+    public void setItemsName(String itemsName) {
+        this.itemsName = itemsName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
