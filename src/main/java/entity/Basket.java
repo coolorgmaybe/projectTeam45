f6f8ee1b566e53e125bbbd74aa560a4c3e3564c4
timofey <gusev.t.s1@gmail.com>
2019-01-26 19:43:02 +0300

    added data model

---
+++ src/main/java/entity/Basket.java
@@ -0,0 +1,73 @@
+package entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+/**
+ * This is class for users basket. This directly connects with users.
+ */
+
+@Entity
+public class Basket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "user_id can not be empty")
+    private String user_id;
+    @NotBlank(message = "item_id can not be empty")
+    private String item_id;
+
+    private Long price;
+    private Long count;
+    private String timeOfOrder;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(String item_id) {
+        this.item_id = item_id;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    public String getTimeOfOrder() {
+        return timeOfOrder;
+    }
+
+    public void setTimeOfOrder(String timeOfOrder) {
+        this.timeOfOrder = timeOfOrder;
+    }
+}
